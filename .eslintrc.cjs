db452275d02362a238fb3f2a1cb6f2b8c5fc5c3f
noguilty4you <34337846+noguilty4you@users.noreply.github.com>
2023-05-10 04:15:34 +0400

    astro works

---
+++ .eslintrc.cjs
@@ -0,0 +1,74 @@
+
+const isProductionLinting = process.env.NODE_ENV === 'production'
+
+module.exports = {
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		project: ['tsconfig.json'],
+		sourceType: 'module',
+	},
+	plugins: ['@typescript-eslint/eslint-plugin'],
+	extends: [
+		'plugin:@typescript-eslint/recommended',
+		'plugin:prettier/recommended',
+    "plugin:astro/recommended",
+	],
+	root: true,
+	env: {
+		node: true,
+		jest: true,
+		browser: true
+	},
+	rules: {
+		'prettier/prettier': [
+			'error',
+			{
+				trailingComma: 'all',
+				semi: false,
+				singleQuote: true,
+				bracketSpacing: true,
+				useTabs: true,
+			},
+		],
+		'quotes': isProductionLinting ? ['error', 'single', { "avoidEscape": true }] : 'off',
+		'indent': isProductionLinting ? ['warn' , 'tab'] : 'off',
+		'no-mixed-spaces-and-tabs': isProductionLinting ? 'error' : 'off',
+		
+		'no-console': isProductionLinting ? 'error' : 'off',
+		'no-debugger': isProductionLinting ? 'error' : 'off',
+		
+		'no-unused-vars': 'off',
+		'@typescript-eslint/no-unused-vars': isProductionLinting ? 'error' : 'off',
+		'no-unused-expressions': isProductionLinting ? 'error' : 'off',
+		
+		'@typescript-eslint/interface-name-prefix': 'off',
+		'@typescript-eslint/explicit-function-return-type': 'off',
+		'@typescript-eslint/explicit-module-boundary-types': 'off',
+		'@typescript-eslint/no-explicit-any': 'off',
+		'@typescript-eslint/ban-ts-comment': isProductionLinting ? 'error' : 'off',
+		
+		// the rule below still does not cover the cases of awaiting in 'for await...of' loops
+		'no-await-in-loop': 'error',
+		
+		'@typescript-eslint/await-thenable': 'error',
+		'@typescript-eslint/no-floating-promises': 'error',
+		
+		// TODO: temporary disabled. talk, decide [and enable]
+		'@typescript-eslint/require-await': 'off',
+		'no-var': 'off'
+	},
+  overrides: [
+    {
+      // Define the configuration for `.astro` file.
+      files: ["**/*.astro"],
+      // Allows Astro components to be parsed.
+      parser: "astro-eslint-parser",
+      // Parse the script in `.astro` as TypeScript by adding the following configuration.
+      // It's the setting you need when using TypeScript.
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+        extraFileExtensions: [".astro"],
+      },
+    },
+  ]
+}
